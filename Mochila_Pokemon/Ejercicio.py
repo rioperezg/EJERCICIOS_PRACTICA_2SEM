3957804c7fe282fbe2f46f21508e23166b029aaa
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-20 18:08:17 +0200

    solucionando problema

---
+++ Mochila_Pokemon/Ejercicio.py
@@ -0,0 +1,20 @@
+"""
+¡Zeraora, Charmander, Bulbasaur, Pikachu y otros Pokemon se han escondido por nuestra Facultad! Ahora es tu oportunidad de descubrirlos y 
+capturarlos. Cada Pokemon tiene un valor (positivo) asociado y estar´a disponible para ser capturado solo durante unos cuantos dıas, 
+pasado este periodo, el Pokemon desaparecer´a para siempre. Teniendo en cuenta que capturar un Pokemon exige un d´ıa completo de b´usqueda, 
+tienes que decidir cuales Pokemon capturar y cuando hacerlo para maximizar el valor total.
+"""
+# importamos las librerías necesarias 
+import pandas
+
+"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+file_path = "sic-avisos_recepcionados_2023.csv"
+with open(file_path, "rb") as f:
+    result = chardet.detect(f.read())
+
+# Leer el archivo CSV utilizando la codificación detectada
+df_avisos = pd.read_csv(file_path, encoding=result["encoding"], on_bad_lines="skip")
+"""
