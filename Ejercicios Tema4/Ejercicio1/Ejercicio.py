e40fc770290773ae152fc93d02c03f33086ffc8b
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-20 19:28:55 +0200

    implementando herramientas

---
+++ Ejercicios Tema4/Ejercicio1/Ejercicio.py
@@ -0,0 +1,10 @@
+"""
+Desarrollar los algoritmos necesarios para generar un árbol de Huffman a partir de la siguiente tabla –para lo cual deberá calcular 
+primero las frecuencias de cada carácter a partir de la can- tidad de apariciones del mismo–, para resolver las siguientes actividades:
+ 
+la generación del árbol debe hacerse desde los caracteres de menor frecuencia hasta los de mayor, en el caso de que dos caracteres tengan 
+la misma frecuencia, primero se toma el que este primero en el alfabeto, el carácter “espacio” y “coma” se consideraran anteúltimo y último 
+respectivamente en el orden alfabético;
+ 
+descomprimir los siguientes mensajes –cuyo árbol ha sido construido de la misma manera que el ejemplo visto anteriormente:
+"""
