64fd5b3276a59d49054d0742c6a50f46271b4ed4
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-23 19:57:16 +0200

    no puedo seguir con el ejercicio3

--- Ejercicios Tema4/Ejercicio3/Ejercicio.py
+++ Ejercicios Tema4/Ejercicio3/Ejercicio.py
@@ -21,3 +21,34 @@ la estación Victoria Train Station hasta la estación Liverpool Street Station,
  
 la estación St. Pancras hasta la estación King's Cross;
 """
+from Herramientas import Cola, Arista, Grafo, nodoVertice, nodoArista
+# Hemos de cargar 6 estaciones y 12 cambios de agujas
+# Cada cambio de aguja debe tener al menos 4 salidas o vertices adyacentes
+# Cada estacion como maximo 2 salidas o llegadas y no puede haber dos estaciones conectadas directamente
+class Tipo_vert(object):
+    def __init__(self):
+        tipo = input("Ingrese el tipo de vertice(estacion/desvio): ")
+        if tipo == "estacion":
+            self.tipo = "estacion"
+        elif tipo == "desvio":
+            self.tipo = "desvio"
+red_ferro = Grafo()
+estacion = nodoVertice(info=input("Ingrese el nombre de la estacion a cargar: "))
+i = 0
+while (i < 6):
+    Tipo_vert(estacion)
+    Arista.insertar_vertice(red_ferro, estacion)
+    i += 1
+    estacion = nodoVertice(info=input("Ingrese el nombre de la estacion a cargar: "))
+desvio = nodoVertice(info=input("Ingrese el nombre del desvio a cargar: "))
+i = 0
+while (i < 12):
+    Tipo_vert(desvio)
+    Arista.insertar_vertice(red_ferro, desvio)
+    i += 1
+    desvio = nodoVertice(info=input("Ingrese el nombre del desvio a cargar: "))
+
+Tamaño = Arista.tamaño(red_ferro)
+while(Tamaño > 0):
+    
+
