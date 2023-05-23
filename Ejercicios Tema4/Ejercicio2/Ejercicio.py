f2e5e3e7f43046a09c86ba9c52d6c01a66049ab4
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-23 18:43:13 +0200

    empezando ejercicio 3

--- Ejercicios Tema4/Ejercicio2/Ejercicio.py
+++ Ejercicios Tema4/Ejercicio2/Ejercicio.py
@@ -22,17 +22,66 @@ durante la atención;
  
 indicar la cantidad total de recursos asignados a las misiones.
 """
-from Herramientas import Cola
+from Herramientas import Cola, Lista, nodoLista  
+# Necesitaremos una funcion que nos devuelva un vehiciulo aleatorio de una lista de vehiculos predefinida
+import random
 
+lista_vehiculos = Lista()
+
+def Lista_vehiculos(vehiculos):
+    if vehiculos == []:
+        print("Ya se han cargado los vehiculos")
+        return lista_vehiculos
+    else:
+        vehiculo = vehiculos[0]
+        vehiculos.pop(vehiculo)
+        Lista.insertar(lista_vehiculos, vehiculo)
+        return Lista_vehiculos(vehiculos)
+list_vehic = Lista_vehiculos(["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST"])
+list_vehic2 = Lista_vehiculos(["AT-AT", "AT-RT", "AT-TE", "AT-DP", "AT-ST", "AT-M6", "AT-MP", "AT-DT"])
 class Mision(object):
     def __init__(self):
         self.tipo = input("(Presione enter si quiere salir) Tipo de misión: ")
         self.Planeta = input("(Presione enter si quiere salir) Planeta de destino: ")
         self.general = input("(Presione enter si quiere salir) General que solicita la msiion: ")
+    def recursos(self, mision):
+        if (mision.prioridad == prioridad_alta):
+            print("los recursos se introduciran manualmente")
+            self.stormtroopers = int(input("stormtroopers > "))
+            self.vehiculos = input("vehiculos > ")
+        else: 
+            print("los recursos se asignara de forma automatica")
+            if (mision.tipo == "exploracion"):
+                self.stormtroopers = 15
+                self.vehiculos = "2 speeder bikes"
+            elif(mision.tipo == "contencion"):
+                self.stormtroopers = 30
+                # Ahora de forma aleatoria de la lista de vehiculos se eligen 3
+                self.vehiculos = random.sample(list_vehic, 3)
+            elif(mision.tipo == "ataque"):
+                self.stormtroopers = 50
+                self.vehiculos = random.sample(list_vehic2, 7)
+i = 0
 prioridad_alta = 1
 prioridad_baja = 0
 mision = Mision()
 misiones = Cola()
 while(mision.tipo != "" or mision.Planeta != "" or mision.general != ""):
     if (mision.general == "Palpatine" or "Darth Vader"):
-        Cola.arribo_con_prioridad(misiones, mision, )
+        Cola.arribo_con_prioridad(misiones, mision, prioridad=prioridad_alta)
+        i += 1
+    else: 
+        Cola.arribo_con_prioridad(misiones, mision, prioridad_baja)
+        i += 1
+    mision = Mision()
+while(i != 0): 
+    dato = Cola.barrido(misiones)
+    if dato.prioridad == prioridad_alta:
+        print("los recursos se introduciran manualmente")
+        stormtroopers = input("stormtroopers > ")
+        vehiculos = input("vehiculos > ")
+        i -= 1
+    else:
+        Mision.recursos(dato)
+        i -= 1
+
---
+++ Ejercicios Tema4/Ejercicio3/Ejercicio.py
@@ -0,0 +1,23 @@
+"""
+Se requiere implementar una red de ferrocarriles compuesta de estaciones de trenes y cambios de agujas (o desvíos). Contemplar las 
+siguientes consideraciones:
+ 
+cada vértice del grafo no dirigido tendrá un tipo (estación o desvió) y su nombre, en el caso de los desvíos el nombre es un número –estos 
+estarán numerados de manera consecutiva–;
+ 
+cada desvío puede tener múltiples puntos de entrada y salida;
+ 
+se deben cargar seis estaciones de trenes y doce cambios de agujas;
+
+cada cambio de aguja debe tener al menos cuatro salida o vértices adyacentes;
+ 
+y cada estación como máximo dos salidas o llegadas y no puede haber dos estaciones co- nectadas directamente;
+ 
+encontrar el camino más corto desde:
+ 
+la estación King's Cross hasta la estación Waterloo,
+ 
+la estación Victoria Train Station hasta la estación Liverpool Street Station,
+ 
+la estación St. Pancras hasta la estación King's Cross;
+"""
