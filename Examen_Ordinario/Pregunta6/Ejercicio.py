06aa9368a48639449e50a9ca3ce0fbf93c07dd63
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-05-24 15:51:29 +0200

    repasando

---
+++ Examen_Ordinario/Pregunta6/Ejercicio.py
@@ -0,0 +1,12 @@
+# Cassian andor maestro del ladron de la galaxia, ha robado de las manos de un stormtrooper una mochila con muchos afectados valiosos 
+# (pensamos que era de un poderoso jedi) pero, llegando a la nave LMTR-20 se da cuenta que para escapar de las garras del malvado darth vader
+# debe desprenderse de algunos artefactos valiosos ya que su nave debe ser mas ligera que un crucero estelar y debe contener una cantidad 
+# limitada de peso. Su objetivo es salir con el valor combinado mas alto de artefactos que quepan en la mochila, pero ¿como elige estos 
+# articulos y cual es el valor optimo?.
+
+#ahora vamos a ser un poco mas precisos y dados los siguientes valores.
+#precio = [103, 60, 70, 5, 15]
+#pesos = [12, 23, 11, 15, 7]
+#peso_maximo = 100
+
+#¿cual es el valor maximo de los articulos que se pueden agregar a la mochila de manera que el peso no exceda el limite de peso W?
---
+++ Examen_Ordinario/Pregunta8/Ejercicio.py
@@ -0,0 +1,7 @@
+#poe dameron, lider del escuadron negro de la resistencia, tiene dificultades para transmitir los mensajes a la base de la resistencia, 
+# dado que los mismos son muy largos y los satelites espias de la primera orden los intercepta, en un lapso muy corto desde que se tranmiten. 
+# Por lo cual, nos solicita desarollar un algoritmo que permita comprimir los mensajes para enviarlos mas rapido y no puedan ser interceptados. 
+# Contemplando los siguientes requerimientos, implementar un algorimto que pueda crear un arbol de huffman a partir de la siguiente tabla y 
+# desarrollar las funciones para comprimir y descomprimir un mensaje.
+#Simbolo(A, F, 1, 3, 0, M, T)
+#frecuencia(0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15)
